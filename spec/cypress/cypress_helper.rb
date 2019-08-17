d879499af97700e7180aad2581ff166e2248548f
Georgy Shabunin <georgy.shabunin@gmail.com>
2019-08-17 17:45:12 +0200

    Add factory bot rails gem
    Add the gem and make it visible to cypress together with
    all the factories from the solidus gem.

--- spec/cypress/cypress_helper.rb
+++ spec/cypress/cypress_helper.rb
@@ -1,1 +1,8 @@
 require 'database_cleaner'
+require "factory_bot"
+require "spree/testing_support/factories"
+require "carmen"
+
+Dir[Rails.root.join("spec", "support", "factories", "**", "*.rb")].each { |f| require f }
+
+FactoryBot.find_definitions
